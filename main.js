2249f4c6d3c80cb4ec3aa77bbf1fee2acc238cf6
GodsplanELot <elotgodsplan2@gmail.com>
2024-10-03 01:46:52 +0100

    wrote logic for rock paper scissors game

---
+++ main.js
@@ -0,0 +1,20 @@
+//Conditionals: if Statements 
+
+if (playerOne === computer) {
+    //tie game!
+} else if (playerOne === "rock"){
+    if (computer === "paper"){
+        // computer wins
+    }else {
+        //playerOne wins
+    }
+
+}   else if (playerOne === "paper"){
+    if (computer === "scissors"){
+        //computer wins
+    } else {
+        //playerOne wins
+    } else {
+        //playerOne wins
+    }
+}
