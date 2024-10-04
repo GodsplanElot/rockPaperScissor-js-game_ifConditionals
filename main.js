1e88e9ac0f0fe7da22745bed46a657ba580ebb48
GodsplanELot <elotgodsplan2@gmail.com>
2024-10-04 10:21:51 +0100

    updated the logic for the game

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
 //Conditionals: if Statements 
 
+let playerOne = "";
+let computer = "";
+
 if (playerOne === computer ) {
     //tie game!
 }else if (playerOne === "rock"){
     if (computer === "paper"){
+        //computer wins
+    } else {
+        //playerOne wins
+    }
+}else if (playerOne == "paper"){
+    if (computer === "scissors"){
+        //computer wins
+    }else {
+        //playerOne wins
+    }
+}else {
+    if (computer == "rock"){
         //computer wins
     } else {
         //playerOne wins
